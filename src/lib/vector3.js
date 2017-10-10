9e493013812169c86f94a9b6c9b422abf39fb557
fallenoak <git@fallenoak.me>
2017-10-09 20:58:29 -0500

    Add fromValues to Vector3

--- src/lib/vector3.js
+++ src/lib/vector3.js
@@ -19,3 +19,24 @@ export function create() {
   return new Float32Array(DEFAULT);
 
 }
+
+/**
+ * Create new vector from given values.
+ *
+ * @param {Number} x The x term
+ * @param {Number} y The y term
+ * @param {Number} z The z term
+ *
+ * @returns {Vector3} The new vector
+ */
+export function fromValues(x, y, z) {
+
+  const vector = create();
+
+  vector[0] = x;
+  vector[1] = y;
+  vector[2] = z;
+
+  return vector;
+
+}
--- src/specs/vector3.spec.js
+++ src/specs/vector3.spec.js
@@ -22,4 +22,24 @@ describe('Vector3', () => {
 
   });
 
+  describe('#fromValues', () => {
+
+    test('returns new vector matching given values', () => {
+
+      const vector = Vector3.fromValues(1.0, 2.0, 3.0);
+
+      expect(vector).toEqual(new Float32Array([1.0, 2.0, 3.0]));
+
+    });
+
+    test('returns new vector matching undefined values', () => {
+
+      const vector = Vector3.fromValues();
+
+      expect(vector).toEqual(new Float32Array([NaN, NaN, NaN]));
+
+    });
+
+  });
+
 });
