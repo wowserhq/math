a85daf8bb404e5c861f51123898e7977ab6d4eec
fallenoak <git@fallenoak.me>
2017-10-09 21:05:53 -0500

    Add majorAxis to Vector3

--- src/lib/vector3.js
+++ src/lib/vector3.js
@@ -10,6 +10,13 @@
 export const DEFAULT = new Float32Array([0, 0, 0]);
 
 /**
+ * Axis constants
+ */
+export const AXIS_X = 0;
+export const AXIS_Y = 1;
+export const AXIS_Z = 2;
+
+/**
  * Create new vector.
  *
  * @returns {Vector3} The new vector
@@ -38,5 +45,35 @@ export function fromValues(x, y, z) {
   vector[2] = z;
 
   return vector;
+
+}
+
+/**
+ * Return the axis with the largest absolute value for the given vector. In cases of
+ * equality, the returned axis is biased toward the right of X, Y, and Z.
+ *
+ * @param {Vector3} vector The vector
+ *
+ * @returns {Number} Axis with the largest value
+ */
+export function majorAxis(vector) {
+
+  const ax = Math.abs(vector[0]);
+  const ay = Math.abs(vector[1]);
+  const az = Math.abs(vector[2]);
+
+  if (az >= ax && az >= ay) {
+
+    return AXIS_Z;
+
+  } else if (ay >= ax) {
+
+    return AXIS_Y;
+
+  } else {
+
+    return AXIS_X;
+
+  }
 
 }
--- src/specs/vector3.spec.js
+++ src/specs/vector3.spec.js
@@ -42,4 +42,78 @@ describe('Vector3', () => {
 
   });
 
+  describe('#majorAxis', () => {
+
+    test('returns x axis when x is major', () => {
+
+      const vector = Vector3.fromValues(3.0, 2.0, 1.0);
+
+      const axis = Vector3.majorAxis(vector);
+
+      expect(axis).toEqual(Vector3.AXIS_X);
+
+    });
+
+    test('returns y axis when y is major', () => {
+
+      const vector = Vector3.fromValues(1.0, 3.0, 2.0);
+
+      const axis = Vector3.majorAxis(vector);
+
+      expect(axis).toEqual(Vector3.AXIS_Y);
+
+    });
+
+    test('returns z axis when z is major', () => {
+
+      const vector = Vector3.fromValues(1.0, 2.0, 3.0);
+
+      const axis = Vector3.majorAxis(vector);
+
+      expect(axis).toEqual(Vector3.AXIS_Z);
+
+    });
+
+    test('returns z axis when x and z are major and equal', () => {
+
+      const vector = Vector3.fromValues(2.0, 1.0, 2.0);
+
+      const axis = Vector3.majorAxis(vector);
+
+      expect(axis).toEqual(Vector3.AXIS_Z);
+
+    });
+
+    test('returns y axis when x and y are major and equal', () => {
+
+      const vector = Vector3.fromValues(2.0, 2.0, 1.0);
+
+      const axis = Vector3.majorAxis(vector);
+
+      expect(axis).toEqual(Vector3.AXIS_Y);
+
+    });
+
+    test('returns z axis when y and z are major and equal', () => {
+
+      const vector = Vector3.fromValues(1.0, 2.0, 2.0);
+
+      const axis = Vector3.majorAxis(vector);
+
+      expect(axis).toEqual(Vector3.AXIS_Z);
+
+    });
+
+    test('returns z axis when all axes are equal', () => {
+
+      const vector = Vector3.fromValues(1.0, 1.0, 1.0);
+
+      const axis = Vector3.majorAxis(vector);
+
+      expect(axis).toEqual(Vector3.AXIS_Z);
+
+    });
+
+  });
+
 });
