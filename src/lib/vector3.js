1eab77792f2da08f70815a81403dcdd1c26b7ba3
fallenoak <git@fallenoak.me>
2017-10-12 17:46:26 -0500

    Return to more conventional line spacing

--- src/lib/vector3.js
+++ src/lib/vector3.js
@@ -2,18 +2,14 @@
  * Axis constants
  *
  * @readonly
- *
  * @memberof Vector3
- *
  * @enum {Number}
  */
 const AXIS = {
   /** X axis */
   X: 0,
-
   /** Y axis */
   Y: 1,
-
   /** Z axis */
   Z: 2
 };
@@ -24,9 +20,7 @@ Object.freeze(AXIS);
  * Default values
  *
  * @readonly
- *
  * @memberof Vector3
- *
  * @type {Array}
  */
 const DEFAULT = [0.0, 0.0, 0.0];
@@ -39,35 +33,27 @@ Object.freeze(DEFAULT);
  * @extends Float32Array
  */
 class Vector3 extends Float32Array {
-
   /**
    * Create a new vector.
    *
    * @param {Array} arr Initial values
    */
   constructor(arr = DEFAULT) {
-
     if (arr && arr.length > 3) {
-
       throw new Error('Invalid length');
-
     }
 
     super(arr);
-
   }
 
   /**
    * Check for exact equality against the given vector.
    *
    * @param {Vector3} v Vector to compare
-   *
    * @returns {Boolean} Equality
    */
   equals(v) {
-
     return this[0] === v[0] && this[1] === v[1] && this[2] === v[2];
-
   }
 
   /**
@@ -77,25 +63,17 @@ class Vector3 extends Float32Array {
    * @returns {Number} Axis with the largest value
    */
   majorAxis() {
-
     const ax = Math.abs(this[0]);
     const ay = Math.abs(this[1]);
     const az = Math.abs(this[2]);
 
     if (az >= ax && az >= ay) {
-
       return AXIS.Z;
-
     } else if (ay >= ax) {
-
       return AXIS.Y;
-
     } else {
-
       return AXIS.X;
-
     }
-
   }
 
   /**
@@ -107,29 +85,20 @@ class Vector3 extends Float32Array {
    * @returns {void}
    */
   static forEach(arr, cb) {
-
     if (arr.length % 3 !== 0) {
-
       throw new Error('Invalid length');
-
     }
 
     const l = arr.length;
-
-    let i;
-
     const v = new Vector3();
 
-    for (i = 0; i < l; i += 3) {
-
+    for (let i = 0; i < l; i += 3) {
       v[0] = arr[i];
       v[1] = arr[i + 1];
       v[2] = arr[i + 2];
 
       cb.call(this, v, i / 3, arr);
-
     }
-
   }
 
   /**
@@ -140,23 +109,15 @@ class Vector3 extends Float32Array {
    * @returns {Iterator} New iterator
    */
   static *keys(arr) {
-
     if (arr.length % 3 !== 0) {
-
       throw new Error('Invalid length');
-
     }
 
     const l = arr.length / 3;
 
-    let i;
-
-    for (i = 0; i < l; i++) {
-
+    for (let i = 0; i < l; i++) {
       yield i;
-
     }
-
   }
 
   /**
@@ -167,17 +128,11 @@ class Vector3 extends Float32Array {
    * @returns {Vector3} New vector
    */
   static of(...args) {
-
     if (args.length === 0) {
-
       return new Vector3();
-
     } else {
-
       return new Vector3(args);
-
     }
-
   }
 
   /**
@@ -188,31 +143,21 @@ class Vector3 extends Float32Array {
    * @returns {Iterator} New iterator
    */
   static *values(arr) {
-
     if (arr.length % 3 !== 0) {
-
       throw new Error('Invalid length');
-
     }
 
     const l = arr.length;
-
-    let i;
-
     const v = new Vector3();
 
-    for (i = 0; i < l; i += 3) {
-
+    for (let i = 0; i < l; i += 3) {
       v[0] = arr[i];
       v[1] = arr[i + 1];
       v[2] = arr[i + 2];
 
       yield v;
-
     }
-
   }
-
 }
 
 Vector3.AXIS = AXIS;
--- src/specs/vector3.spec.js
+++ src/specs/vector3.spec.js
@@ -1,175 +1,122 @@
 import { Vector3 } from '../lib';
 
 describe('Vector3', () => {
-
   describe('prototype.constructor()', () => {
-
     test('returns new vector with expected type', () => {
-
       const vector = new Vector3();
 
       expect(vector).toBeInstanceOf(Vector3);
       expect(vector).toBeInstanceOf(Float32Array);
-
     });
 
     test('returns new vector with default values', () => {
-
       const vector = new Vector3();
 
       expect(vector.equals(Vector3.DEFAULT)).toBe(true);
-
     });
-
   });
 
   describe('prototype.majorAxis()', () => {
-
     test('returns x axis when x is major', () => {
-
       const vector = Vector3.of(3.0, 2.0, 1.0);
-
       const axis = vector.majorAxis(vector);
 
       expect(axis).toEqual(Vector3.AXIS.X);
-
     });
 
     test('returns y axis when y is major', () => {
-
       const vector = Vector3.of(1.0, 3.0, 2.0);
-
       const axis = vector.majorAxis(vector);
 
       expect(axis).toEqual(Vector3.AXIS.Y);
-
     });
 
     test('returns z axis when z is major', () => {
-
       const vector = Vector3.of(1.0, 2.0, 3.0);
-
       const axis = vector.majorAxis(vector);
 
       expect(axis).toEqual(Vector3.AXIS.Z);
-
     });
 
     test('returns z axis when x and z are major and equal', () => {
-
       const vector = Vector3.of(2.0, 1.0, 2.0);
-
       const axis = vector.majorAxis(vector);
 
       expect(axis).toEqual(Vector3.AXIS.Z);
-
     });
 
     test('returns y axis when x and y are major and equal', () => {
-
       const vector = Vector3.of(2.0, 2.0, 1.0);
-
       const axis = vector.majorAxis(vector);
 
       expect(axis).toEqual(Vector3.AXIS.Y);
-
     });
 
     test('returns z axis when y and z are major and equal', () => {
-
       const vector = Vector3.of(1.0, 2.0, 2.0);
-
       const axis = vector.majorAxis(vector);
 
       expect(axis).toEqual(Vector3.AXIS.Z);
-
     });
 
     test('returns z axis when all axes are equal', () => {
-
       const vector = Vector3.of(1.0, 1.0, 1.0);
-
       const axis = vector.majorAxis(vector);
 
       expect(axis).toEqual(Vector3.AXIS.Z);
-
     });
-
   });
 
   describe('forEach()', () => {
-
     test('iterates over array', () => {
-
       const arr = new Float32Array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
-
       let last;
 
       Vector3.forEach(arr, function(v, i) {
-
         last = i;
-
       });
 
       expect(last).toBe(1);
-
     });
 
     test('permits manipulation of array', () => {
-
       const arr = new Float32Array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
 
       Vector3.forEach(arr, function(v, i) {
-
         v[0] = 0.0;
 
         arr.set(v, i * 3);
-
       });
 
       expect(arr[0]).toBe(0.0);
       expect(arr[3]).toBe(0.0);
-
     });
-
   });
 
   describe('keys()', () => {
-
     test('returns vector iterator', () => {
-
       const arr = new Float32Array([1.0, 2.0, 3.0]);
-
       const iterator = Vector3.keys();
 
       expect(iterator.next).toBeInstanceOf(Function);
-
     });
 
     test('iterates over array as vector indices', () => {
-
       const arr = new Float32Array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
-
       let last;
 
       for (const k of Vector3.keys(arr)) {
-
         last = k;
-
       }
 
       expect(last).toBe(1);
-
     });
 
     test('permits manipulation of array', () => {
-
       const arr = new Float32Array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
-
       const v = new Vector3();
 
       for (const k of Vector3.keys(arr)) {
-
         v[0] = arr[k * 3];
         v[1] = arr[k * 3 + 1];
         v[2] = arr[k * 3 + 2];
@@ -177,64 +124,44 @@ describe('Vector3', () => {
         v[1] *= 2.0;
 
         arr.set(v, k * 3);
-
       }
 
       expect(arr[1]).toBe(4.0);
       expect(arr[4]).toBe(10.0);
-
     });
-
   });
 
   describe('of()', () => {
-
     test('returns new vector matching given values', () => {
-
       const vector = Vector3.of(1.0, 2.0, 3.0);
 
       expect(vector.equals([1.0, 2.0, 3.0])).toBe(true);
-
     });
 
     test('returns new default vector when not given values', () => {
-
       const vector = Vector3.of();
 
       expect(vector.equals(Vector3.DEFAULT)).toBe(true);
-
     });
-
   });
 
   describe('values()', () => {
-
     test('returns vector iterator', () => {
-
       const arr = new Float32Array([1.0, 2.0, 3.0]);
-
       const iterator = Vector3.values();
 
       expect(iterator.next).toBeInstanceOf(Function);
-
     });
 
     test('iterates over array as vectors', () => {
-
       const arr = new Float32Array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
-
       let last;
 
       for (const v of Vector3.values(arr)) {
-
         last = v;
-
       }
 
       expect(last.equals([4.0, 5.0, 6.0])).toBe(true);
-
     });
-
   });
-
 });
