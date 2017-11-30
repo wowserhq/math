ae940bc77202e59ea512cc1136adf5e4a636f657
fallenoak <git@fallenoak.me>
2017-11-29 23:34:02 -0600

    Add common constants

---
+++ src/lib/common.js
@@ -0,0 +1,8 @@
+const DEG2RAD = Math.PI / 180.0;
+
+const EPSILON = 0.0001;
+
+export {
+  DEG2RAD,
+  EPSILON
+};
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,11 @@
+import { DEG2RAD, EPSILON } from './common';
 import Matrix4 from './matrix4';
 import * as Plane from './plane';
 import Vector3 from './vector3';
 
 export {
+  DEG2RAD,
+  EPSILON,
   Matrix4,
   Plane,
   Vector3
