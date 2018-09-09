cba94eaac95de1cee05921552905c2e5f316208c
fallenoak <git@fallenoak.me>
2018-09-09 10:42:57 -0500

    Update package structure and build tooling
    * Switched build output to commonjs (.js) and esm (.mjs) modules
    * Removed webpack
    * Updated to Babel 7
    * Adjusted filenames of class modules to use pascal casing

---
+++ src/lib/index.mjs
@@ -0,0 +1,14 @@
+import { DEG2RAD, EPSILON } from './common';
+import Matrix4 from './Matrix4';
+import Plane from './Plane';
+import Quaternion from './Quaternion';
+import Vector3 from './Vector3';
+
+export {
+  DEG2RAD,
+  EPSILON,
+  Matrix4,
+  Plane,
+  Quaternion,
+  Vector3,
+};
