e1eaf57a162ee9c6eb6c512963c224b3cb5ad084
fallenoak <git@fallenoak.me>
2020-05-24 14:21:21 -0500

    chore: switch to babel.config.js (#49)

---
+++ babel.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    cjs: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: '10',
+              browsers: [
+                'last 2 versions',
+                'not ie 1-11',
+                'not ie_mob > 0',
+                'not android > 0',
+              ],
+            },
+          },
+        ],
+      ],
+    },
+
+    esm: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: '10',
+              browsers: [
+                'last 2 versions',
+                'not ie 1-11',
+                'not ie_mob > 0',
+                'not android > 0',
+              ],
+            },
+            modules: false,
+          },
+        ],
+      ],
+    },
+
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: '10',
+            },
+          },
+        ],
+      ],
+    },
+  },
+};
